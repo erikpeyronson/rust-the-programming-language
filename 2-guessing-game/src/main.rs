23027fdf1fb97ff35225c2cd9c2526c72adbc665
Erik Peyronson <erikpeyronson@gmail.com>
2020-06-14 18:14:01 +0200

    Add validation with Guess type in guessing-game

--- 2-guessing-game/src/main.rs
+++ 2-guessing-game/src/main.rs
@@ -2,6 +2,22 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess must be between 1 and 100, got {}", value);
+        }
+        Guess { value }
+    }
+    fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
@@ -16,14 +32,14 @@ fn main() {
             .read_line(&mut guess)
             .expect("failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
+        let guess: Guess = match guess.trim().parse() {
+            Ok(num) => Guess::new(num),
             Err(_) => continue,
         };
 
-        println!("You guessed {}", guess);
+        println!("You guessed {}", guess.value());
 
-        match guess.cmp(&secret_number) {
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {

34a2a47a48672e2e1905ed9c20a5f97c0fb378e5
Erik Peyronson <erikpeyronson@gmail.com>
2020-06-27 10:03:12 +0200

    Finish minigrep

--- 12-io-project/minigrep/src/lib.rs
+++ 12-io-project/minigrep/src/lib.rs
@@ -1,9 +1,11 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -14,24 +16,63 @@ impl Config {
 
         let query = args[1].clone();
         let filename = args[2].clone();
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config { query, filename })
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn test_new() {
+    fn new() {
         let config = Config::new(&[
             String::from("minigrep"),
             String::from("test_arg"),
@@ -44,7 +85,7 @@ mod tests {
     }
 
     #[test]
-    fn test_new_error_when_not_enough_args() {
+    fn new_error_when_not_enough_args() {
         let result = Config::new(&[String::from("minigrep"), String::from("test_arg")]);
         assert!(
             result.is_err(),
@@ -53,7 +94,7 @@ mod tests {
     }
 
     #[test]
-    fn test_run_fails_when_file_is_missing() {
+    fn run_fails_when_file_is_missing() {
         let config = Config::new(&[
             String::from("minigrep"),
             String::from("test_arg"),
@@ -65,5 +106,30 @@ mod tests {
             result.is_err(),
             "Run did not return error when file is missing"
         )
+    }
+
+    #[test]
+    fn search_sensitive() {
+        let query = "duct";
+        let contents = "
+Rust:
+safe, fast, productive
+Pick three.
+Duct tape";
+        assert_eq!(vec!["safe, fast, productive"], search(query, contents));
+    }
+
+    #[test]
+    fn search_insensitive() {
+        let query = "rUsT";
+        let contents = "
+Rust:
+safe, fast, productive
+Pick three.
+Trust me";
+        assert_eq!(
+            vec!["Rust:", "Trust me"],
+            search_case_insensitive(query, contents)
+        );
     }
 }

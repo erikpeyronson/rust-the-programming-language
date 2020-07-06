642053f98723b14303a5d0f57a5ab5a19259cee3
Erik Peyronson <erikpeyronson@gmail.com>
2020-07-06 06:12:43 +0200

    Add channels

---
+++ 16-fearless-concurrency/channels/src/main.rs
@@ -0,0 +1,51 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("Hi from the first thread");
+        tx.send(val).unwrap();
+        // Below wont compile since val is moved in send
+        // println!("Val is: {}", val);
+    });
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = mpsc::Sender::clone(&tx);
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("Hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("second"),
+            String::from("thread"),
+        ];
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("Some"),
+            String::from("more"),
+            String::from("messanges"),
+            String::from("for"),
+            String::from("you"),
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
